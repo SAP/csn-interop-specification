bf4a0f9f6681512eb028f70680cd74a3a31d2d56
raluca.gruber <raluca.gruber@sap.com>
2025-02-10 11:05:41 +0100

    delete unused interfaces

--- spec-toolkit/src/model/Config.ts
+++ spec-toolkit/src/model/Config.ts
@@ -1,47 +1,5 @@
 import fs from "fs-extra";
 import path from "path";
-
-//////////////////////////////////////////
-// NEW CONFIG (TODO, WIP)               //
-//////////////////////////////////////////
-
-export interface SpecToolkitConfig {
-  specs?: SpecConfig[];
-  specExtensions?: ExtensionConfig[];
-  // specOutput?: {};
-  examples?: ExampleConfig[];
-}
-
-// Configuration Document Interface
-export interface SpecConfig {
-  id: string;
-  title: string;
-  sourceFile: string;
-  sourceFileIntroduction: string;
-  targetFile: string;
-  targetFolder?: string;
-  extensionTargetFile?: string;
-  sideBarPosition: number;
-  sideBarDescription: string;
-  /** List of bullet points to add at the top as quick facts / links (in markdown) */
-  facts?: string[];
-}
-
-export interface ExtensionConfig {
-  targetDocument: string;
-  targetDocumentFileName: string;
-  targetDocumentFolder: string;
-  specExtensions: string[]; // array of file paths
-}
-
-export interface ExampleConfig {
-  folder: string; // TODO: Point to folder? Or just give a glob pattern?
-}
-
-//////////////////////////////////////////
-// ORIGINAL CONFIG                      //
-//////////////////////////////////////////
-
 export interface ConfigFile {
   generalConfig: GeneralConfig;
   docsConfig: DocsConfig[];
@@ -73,16 +31,6 @@ export interface DocsConfig {
 
   /** List of bullet points to add at the top as quick facts / links (in markdown) */
   facts?: string[];
-}
-
-//Get Config for a given ID
-export function getConfigForId(docsConfigs: DocsConfig[], id: string): DocsConfig | null {
-  for (let i = 0; i < docsConfigs.length; i++) {
-    if (docsConfigs[i].id === id) {
-      return docsConfigs[i];
-    }
-  }
-  return null;
 }
 
 //Retrieve Text from Introduction File
