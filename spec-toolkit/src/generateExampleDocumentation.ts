dc97c0100a7d3e9ae362b17dc411a0fb696275fd
raluca.gruber <raluca.gruber@sap.com>
2025-02-14 16:07:47 +0100

    spec-toolkit refactor examples documentation generation if examples were provided fro specifications

--- spec-toolkit/src/generateExampleDocumentation.ts
+++ spec-toolkit/src/generateExampleDocumentation.ts
@@ -2,65 +2,60 @@ import fg from "fast-glob";
 import fs from "fs-extra";
 import * as path from "path";
 import { log } from "./util/log.js";
+import { ConfigFile } from "./model/Config.js";
 
 interface ExampleDocumentsDict {
   [fileName: string]: string;
 }
 
-export function generateExampleDocumentation(examplesFolder: string, specTitle: string, outputFolder: string): void {
-  // .jsonc files are ignored. We can use them to create quick mock examples including comments that are not fully validated
-  const csnInteropEffectiveFilePaths = fg.sync(`${examplesFolder}/*.json`, { ignore: ["_*"] });
+export function generateExampleDocumentation(configData: ConfigFile): void {
+  for (const docConfig of configData.docsConfig) {
+    if (docConfig.type === "spec" && docConfig.examples) {
+      const jsonExampleFilePaths = fg.sync(`${docConfig.examples.sourceJsonFolderPath}/*.json`, { ignore: ["_*"] });
 
-  const documentExamplePages = generateExampleDocumentationPage(csnInteropEffectiveFilePaths, specTitle);
-  for (const fileName in documentExamplePages) {
-    const fileContent = documentExamplePages[fileName];
-    fs.outputFileSync(path.join(outputFolder, fileName), fileContent);
-    log.info("Written: " + path.join(outputFolder, fileName));
-  }
-}
+      const mdExamplePages: ExampleDocumentsDict = {};
 
-function generateExampleDocumentationPage(filePaths: string[], documentType: string): ExampleDocumentsDict {
-  const examplePages: ExampleDocumentsDict = {};
-  for (const filePath of filePaths) {
-    let text = "";
-    const exampleFileContent = fs.readFileSync(filePath).toString();
-    const exampleParsed = JSON.parse(exampleFileContent);
-    const fileName = path.parse(filePath).name + ".md";
+      // for each .json example generate a documentation .md site
+      for (const filePath of jsonExampleFilePaths) {
+        let text = "";
+        const exampleFileContent = fs.readFileSync(filePath).toString();
+        const fileName = path.parse(filePath).name + ".md";
 
-    // const title = exampleParsed.meta?.document?.title || path;
-    const title = path.parse(filePath).name;
-    const description = exampleParsed.meta?.document?.doc || `Example documents for ${documentType}.`;
+        const title = path.parse(filePath).name;
+        const description = `Example documents for ${docConfig.id}.`;
 
-    let prefix = "";
-    try {
-      prefix = fs.readFileSync(filePath.replace(".json", ".md")).toString() + "\n\n";
-    } catch (_) {
-      // Ignore
+        let prefix = "";
+        try {
+          prefix = fs.readFileSync(filePath.replace(".json", ".md")).toString() + "\n\n";
+        } catch (_) {
+          // Ignore
+        }
+
+        if (prefix) {
+          text += prefix;
+        } else {
+          text += `---\n`;
+          text += `title: ${title}\n`;
+          // text += `sidebar_position: 2\n`;
+          text += `description: ${description}\n`;
+          text += `---\n\n`;
+          text += `# Example: ${title} \n\n`;
+        }
+
+        log.info(`Found ${docConfig.id} example file: ${filePath}`);
+        text += `## Example File\n\n`;
+        text += "```js\n";
+        text += exampleFileContent;
+        text += "\n```\n";
+
+        mdExamplePages[fileName] = text;
+      }
+
+      for (const fileName in mdExamplePages) {
+        const fileContent = mdExamplePages[fileName];
+        fs.outputFileSync(path.join(docConfig.examples.targetMarkdownFolderPath, fileName), fileContent);
+        log.info("Written: " + path.join(docConfig.examples.targetMarkdownFolderPath, fileName));
+      }
     }
-
-    if (prefix) {
-      text += prefix;
-    } else {
-      text += `---\n`;
-      text += `title: ${title}\n`;
-      // text += `sidebar_position: 2\n`;
-      text += `description: ${description}\n`;
-      text += `---\n\n`;
-      text += `# Example: ${title} \n\n`;
-    }
-
-    log.info(`Found ${documentType} example file: ${filePath}`);
-    text += `## Example File\n\n`;
-    text += `> Source Code: [${filePath}](${filePath.replace(
-      "./",
-      "https://github.com/SAP/csn-interop-specification/blob/main/",
-    )})\n\n`;
-    text += "```js\n";
-    text += exampleFileContent;
-    text += "\n```\n";
-
-    examplePages[fileName] = text;
   }
-
-  return examplePages;
 }
