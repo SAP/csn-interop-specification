5ea1643fe39e7910800f78c4f5f3e4903299b87a
Simon Heimler <simon.heimler@sap.com>
2025-03-12 10:17:31 +0100

    Add types
    Signed-off-by: Simon Heimler <simon.heimler@sap.com>

--- src/types/v1/CSN-Interop-Effective.ts
+++ src/types/v1/CSN-Interop-Effective.ts
@@ -2220,12 +2220,11 @@ export interface CardinalityObject {
  */
 export interface StructuredElementReference {
   /**
-   * Description of the target with *association name* and *target element name* in target entity`
-   * Description of the source *source element name*
+   * Reference to external target with *association name* and *target element name* in `target` entity (2 array items)
+   * Reference to local *source element name* (1 array item).
    *
    * MUST NOT:
-   * - use $ as leading character of an element
-   * - use session variables
+   * - use $ as leading character of an element (e.g. for session variables)
    */
   ref: string[];
 }
