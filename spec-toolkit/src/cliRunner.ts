daa1d034c33b72f70b2e15d61971d7bc2e817d0a
raluca.gruber <raluca.gruber@sap.com>
2025-03-21 14:20:57 +0100

    code review findings

--- spec-toolkit/src/cliRunner.ts
+++ spec-toolkit/src/cliRunner.ts
@@ -1,5 +1,6 @@
 import { Command, Option } from "commander";
 import * as packageJson from "../package.json" with { type: "json" };
+import { readFileSync } from "node:fs";
 import { generate } from "./generate.js";
 import path from "node:path";
 import { ConfigFile } from "./model/Config.js";
@@ -8,14 +9,16 @@ interface CliOptions {
   config: string;
 }
 
+const DEFAULT_CONFIG_FILE_NAME = "./spec-toolkit.config.json";
+
 /**
  * Executes the CLI with additional command line arguments (argv)
  */
 function init(argv: string[]): void {
   const configFilePath = new Option(
     "-c, --config <configFilePath>",
-    "path to spec-toolkit config file (default: `./spec-toolkit.config.js`)",
-  ).default("spec-toolkit.config.js");
+    `path to spec-toolkit config file (default: ${DEFAULT_CONFIG_FILE_NAME})`,
+  ).default(DEFAULT_CONFIG_FILE_NAME);
 
   const program = new Command();
   program
@@ -30,11 +33,16 @@ function init(argv: string[]): void {
 }
 
 async function run(argv: CliOptions): Promise<void> {
-  let configJsModule;
+  let configData: unknown;
   const configFilePath = path.join(process.cwd(), argv.config);
 
   try {
-    configJsModule = await import(`file:///${configFilePath}`);
+    if (configFilePath.endsWith(".json")) {
+      const configFileContent = readFileSync(configFilePath, "utf-8");
+      configData = JSON.parse(configFileContent);
+    } else {
+      throw new Error(`Unsupported file extension: ${configFilePath}`);
+    }
   } catch (error) {
     process.stderr.write(`[error]: ${error}\n\n`);
     process.exit(1);
@@ -43,7 +51,7 @@ async function run(argv: CliOptions): Promise<void> {
   try {
     // TODO: Validate configData against spec-toolkit config JSON schema(tbd) before casting it as ConfigFile
     // throw error if validation fails
-    await generate(configJsModule.default as ConfigFile);
+    await generate(configData as ConfigFile);
   } catch (error) {
     process.stderr.write(`${error}\n\n`);
     process.exit(1);
