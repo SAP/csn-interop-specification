18bcdb9d6b8d916e6ec01dfe45e08fc5874c1302
raluca.gruber <raluca.gruber@sap.com>
2025-03-28 13:55:52 +0100

    fix code review findings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,6 +44,20 @@ const config = {
 
   scripts: ["/csn-interop-specification/js/custom.js"],
 
+  plugins: [
+    [
+      "@docusaurus/plugin-client-redirects",
+      {
+        redirects: [
+          {
+            from: "/csn-interop-specification/annotations",
+            to: "/csn-interop-specification/spec-v1/extensions",
+          },
+        ],
+      },
+    ],
+  ],
+
   themes: [
     "@docusaurus/theme-mermaid",
     [
